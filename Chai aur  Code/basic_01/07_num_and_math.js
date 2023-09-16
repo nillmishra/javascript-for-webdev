11f6a744ef68238aa15d35def58b6f3c833bf67c
Nillotpal Mishra <nillmishra09@gmail.com>
2023-09-17 00:23:14 +0530

    math and num

---
+++ Chai aur  Code/basic_01/07_num_and_math.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+console.log(typeof (balance));
+
+const anotherNumber = 123.4535
+console.log(anotherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.8));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(5.9));
+console.log(Math.min(4,8,9,0));
+console.log(Math.random()); //value 0 to 1
+console.log((Math.random()*10)); 
+console.log((Math.random()*10)+1); 
+ 
