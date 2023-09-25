8afd5d41813f7c863f12e2d3bea8d429e9039539
Nillotpal Mishra <nillmishra09@gmail.com>
2023-09-25 12:20:43 +0530

    while and do while

---
+++ Chai aur  Code/05_itteration/two.js
@@ -0,0 +1,21 @@
+
+let i =0
+while (i<=10) {
+    console.log(`Value of index is ${i}`);
+    i = i+2 
+}
+
+let myArray = ["flash", "superman", "batman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr++
+}
+
+
+let h = 11
+do {
+    console.log(`Score is ${h}`);
+    h++
+} while (h<=10);
