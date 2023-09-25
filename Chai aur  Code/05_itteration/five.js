bc490356a869bc9d8fa6ea721be42b46f82c7ee7
Nillotpal Mishra <nillmishra09@gmail.com>
2023-09-25 12:21:44 +0530

    for each function

---
+++ Chai aur  Code/05_itteration/five.js
@@ -0,0 +1,35 @@
+const coding = ["js", "cpp", "py", "java","rb"]
+
+coding.forEach( function (item){
+    console.log(item);
+})
+
+coding.forEach((item)=>{
+    console.log(item);
+})
+
+function printMe(item){
+    console.log(item);
+}
+coding.forEach(printMe)
+
+coding.forEach((item, index, arr)=>{
+    console.log(item , index, arr);
+})
+
+const myCoding = [{
+    languageName: "javascript",
+    languageFileName: "js"
+},
+{
+    languageName: "python",
+    languageFileName: "py"
+},
+{
+    languageName: "c++",
+    languageFileName: "cpp"
+}
+]
+myCoding.forEach((item)=>{
+    console.log(item);
+})
