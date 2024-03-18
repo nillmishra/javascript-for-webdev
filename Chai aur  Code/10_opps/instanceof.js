b42af83290d629a9c81910876c51ebf52bac6465
Nillotpal Mishra <nillmishra09@gmail.com>
2024-03-18 23:41:29 +0530

    instance

---
+++ Chai aur  Code/10_opps/instanceof.js
@@ -0,0 +1,13 @@
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  const auto = new Car('Honda', 'Accord', 1998);
+  
+  console.log(auto instanceof Car);
+  // Expected output: true
+  
+  console.log(auto instanceof Object);
+  // Expected output: true
+  
